da8ccb030653b610d495be7d1ba141e6e1afffd7
leofa <a.f.unver@gmail.com>
2015-11-02 17:43:14 +0100

    update shell prompt

--- .shell_prompt.sh
+++ .shell_prompt.sh
@@ -45,7 +45,7 @@ function __promptline_cwd {
   local first_char
   local part_count=0
   local formatted_cwd=""
-  local dir_sep="/ "
+  local dir_sep="  "
   local tilde="~"
 
   local cwd="${PWD/#$HOME/$tilde}"
